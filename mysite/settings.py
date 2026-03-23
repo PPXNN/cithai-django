656791d26d42373c8a5bb8988df0599e070327e5
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-03-24 01:56:22 +0700

    Register API routes and add DRF to INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'sharelink',
     'song',
     'songgenerationrequest',
-    'user'
+    'user',
 
 ]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from user.views import search_user
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search-user/', search_user)
+    path("api/", include("song.urls")),
+    path("api/", include("user.urls")),
+    path("api/", include("songgenerationrequest.urls")),
+    path("api/", include("sharelink.urls")),
 ]
