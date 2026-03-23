1989648805b64b233dcb14541251318074b6db66
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-03-23 15:07:59 +0700

    Refactor Song model to use shared enums

--- song/models.py
+++ song/models.py
@@ -1,36 +1,9 @@
 from django.db import models
+from song.enums import Occasion, Genre, VoiceType, Mood
 
 
 # Create your models here.
-class Song(models.Model):
-    class Occasion(models.TextChoices):
-        BIRTHDAY = "birthday", "birthday"
-        WEDDING = "wedding", "wedding"
-        ANNIVERSARY = "anniversary", "anniversary"
-        HOLIDAY = "holiday", "holiday"
-        OTHER = "other", "other"
-    
-    class Genre(models.TextChoices):
-        POP = "pop", "pop"
-        ROCK = "rock", "rock"
-        JAZZ = "jazz", "jazz"
-        CLASSICAL = "classical", "classical"
-        HIPHOP = "hiphop", "hiphop"
-        RB = "rb", "rb"
-        FOLK = "folk", "folk"
-    
-    class VoiceType(models.TextChoices):
-        MALE = "male", "male"
-        FEMALE = "female", "female"
-        INSTRUMENTAL = "instrumental", "instrumental"
-    
-    class Mood(models.TextChoices):
-        HAPPY = "happy", "happy"
-        SAD = "sad", "sad"
-        ROMANTIC = "romantic", "romantic"
-        CALM = "calm", "calm"
-        FUNNY = "funny", "funny"
-    
+class Song(models.Model): 
     class SongStatus(models.TextChoices):
         GENERATING = "generating", "generating"
         COMPLETED = "completed", "completed"
