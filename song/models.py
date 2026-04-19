163e36e7d1c9248fab3a59e4850490331c20cb2a
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-19 15:28:46 +0700

    Remove SongStatus choices and associated status field from Song model

--- song/models.py
+++ song/models.py
@@ -4,10 +4,6 @@ from song.enums import Occasion, Genre, VoiceType, Mood
 
 # Create your models here.
 class Song(models.Model): 
-    class SongStatus(models.TextChoices):
-        GENERATING = "generating", "generating"
-        COMPLETED = "completed", "completed"
-        FAILED = "failed", "failed"
 
     user = models.ForeignKey(
         "user.User",
@@ -15,13 +11,19 @@ class Song(models.Model):
         related_name="song"
         )
     
+    song_generation_request = models.OneToOneField(
+        "songgenerationrequest.SongGenerationRequest",
+        on_delete=models.CASCADE,
+        related_name="song",
+        default=-1
+    )
+    
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
     occasion = models.CharField(max_length=20, choices=Occasion.choices, default=Occasion.OTHER)
     genre = models.CharField(max_length=10, choices=Genre.choices, default=Genre.POP)
     voice_type = models.CharField(max_length=13, choices=VoiceType.choices, default=VoiceType.MALE)
     mood = models.CharField(max_length=10, choices=Mood.choices, default=Mood.HAPPY)
-    status = models.CharField(max_length=11, choices=SongStatus.choices, default=SongStatus.GENERATING)
     audiofile_url = models.CharField(max_length=500, blank=True, default="")
     is_private = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
