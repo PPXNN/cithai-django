97d9027a67570c48f95364fb3979d83471bdaa6f
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-19 15:27:57 +0700

    Refactor SharelinkViewSet to use APIView for CRUD operations

--- sharelink/views.py
+++ sharelink/views.py
@@ -1,7 +1,42 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Sharelink
 from .serializers import SharelinkSerializer
 
-class SharelinkViewSet(viewsets.ModelViewSet):
-    queryset = Sharelink.objects.all()
-    serializer_class = SharelinkSerializer
+class SharelinkViewSet(APIView):
+    def get(self, request, pk=None): # Get data or search data
+        if pk:
+            #Get by pk
+            sharelink = Sharelink.objects.get(pk=pk)
+            serializer = SharelinkSerializer(sharelink)
+        else:
+            sharelinks = Sharelink.objects.all() # List of object
+            serializer = SharelinkSerializer(sharelinks, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self, request): 
+        serializer = SharelinkSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        sharelink = Sharelink.objects.get(pk=pk)
+        serializer = SharelinkSerializer(sharelink, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        sharelink = Sharelink.objects.get(pk=pk)
+        sharelink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
