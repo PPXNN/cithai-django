cbcc6bed0e8e329e63f8e97e66c6a858492be61e
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-19 15:33:00 +0700

    Refactor user views to implement APIView for CRUD operations

--- user/views.py
+++ user/views.py
@@ -1,7 +1,42 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserAPIView(APIView):
+    def get(self, request, pk=None): # Get data or search data
+        if pk:
+            #Get by pk
+            user = User.objects.get(pk=pk)
+            serializer = UserSerializer(user)
+        else:
+            users = User.objects.all() # List of object
+            serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
+    
+    def post(self, request): 
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
