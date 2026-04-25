ccd6521e998569e340cdd01bb08d204d9edfaac7
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-25 23:13:57 +0700

    Update user URL patterns to include new authentication endpoints for Google sign-in and user management, enhancing the authentication flow and user experience.

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AuthSignInAPIView, AuthSignUpAPIView, UserAPIView
+from .views import AuthGoogleAPIView, AuthGoogleRedirectAPIView, AuthMeAPIView, AuthSignInAPIView, AuthSignOutAPIView, AuthSignUpAPIView, UserAPIView
 
 app_name = "user"
 
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', UserAPIView.as_view()),
     path('<int:pk>/', UserAPIView.as_view()),
     path("auth/signup/", AuthSignUpAPIView.as_view()),
+    path("auth/me/", AuthMeAPIView.as_view()),
     path("auth/signin/", AuthSignInAPIView.as_view()),
+    path("auth/signout/", AuthSignOutAPIView.as_view()),
+    path("auth/google/", AuthGoogleAPIView.as_view()),
+    path("auth/google/redirect/", AuthGoogleRedirectAPIView.as_view()),
 ]
