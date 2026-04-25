422e95066c9607fca26c1bfda9953878b4cbb8cf
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-25 10:21:36 +0700

    Add authentication URL patterns for sign in and sign up in user app

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserAPIView
+from .views import AuthSignInAPIView, AuthSignUpAPIView, UserAPIView
 
 app_name = "user"
 
 urlpatterns = [
     path('', UserAPIView.as_view()),
-    path('<int:pk>/', UserAPIView.as_view())
+    path('<int:pk>/', UserAPIView.as_view()),
+    path("auth/signup/", AuthSignUpAPIView.as_view()),
+    path("auth/signin/", AuthSignInAPIView.as_view()),
 ]
