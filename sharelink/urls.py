31025e5e69d33da38af9c4d5138d2aeb7565c511
Thapanan Suwansukhum <thapanansu555@gmail.com>
2026-04-25 10:20:03 +0700

    Add public sharelink redirect URL to routing

--- sharelink/urls.py
+++ sharelink/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SharelinkViewSet
+from .views import SharelinkViewSet, public_sharelink_redirect
 
 app_name = "sharelinks"
 
 urlpatterns = [
     path('', SharelinkViewSet.as_view()),
-    path('<int:pk>/', SharelinkViewSet.as_view())
+    path('<int:pk>/', SharelinkViewSet.as_view()),
+    path("public/<int:pk>/", public_sharelink_redirect, name="public_sharelink"),
 ]
